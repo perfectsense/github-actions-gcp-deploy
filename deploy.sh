d2f2879f7e4a5bd4efd9fa749781415d93083dea
Andrew Miller <andrew@perfectsensedigital.com>
2021-05-14 15:27:43 -0400

    Fix credentials path

--- deploy.sh
+++ deploy.sh
@@ -47,8 +47,8 @@ fi
 
 # BEGIN fold/timer support
 
-openssl des3 -d -in ./.github/deploy/gcp-deploy.json.des3 -out ./.github/deploy/gcp-deploy.json -pass pass:$GCP_CREDENTIALS
-gcloud auth activate-service-account --key-file=.github/deploy/gcp-deploy.json
+openssl des3 -d -in ./.github/gcp-deploy.json.des3 -out ./.github/gcp-deploy.json -pass pass:$GCP_CREDENTIALS
+gcloud auth activate-service-account --key-file=.github/gcp-deploy.json
 
 activity=""
 timer_id=""
